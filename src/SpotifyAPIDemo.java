5faecd1bf6750baa963874cec54845f45f6fe0a7
dlin <david.lin@smes.org>
2024-10-30 14:08:44 -0700

    more spotty

--- src/SpotifyAPIDemo.java
+++ src/SpotifyAPIDemo.java
@@ -4,6 +4,7 @@ import java.net.http.HttpResponse.BodyHandlers;
 
 import org.json.*;
 
+import java.util.Scanner;
 public class SpotifyAPIDemo {
 
     public static void main(String[] args) {
@@ -19,15 +20,38 @@ public class SpotifyAPIDemo {
 
             URI uri = new URI("https://accounts.spotify.com/api/token");
 
+            //token
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(uri)
                     .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials&client_id=" + client_id + "&client_secret=" + client_secret))
                     .header("content-type", "application/x-www-form-urlencoded")
                     .build();
-
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
             System.out.println(response);
             System.out.println(response.body());
+            JSONObject resp = new JSONObject(response.body());
+            String token = resp.getString("access_token");
+
+            Scanner s = new Scanner(System.in);
+            System.out.println("enter artist name");
+            String term = s.nextLine();
+            term = term.replaceAll(" ", "%20");
+
+            //get request for api calls to search for artist
+            uri = new URI("https://api.spotify.com/v1/search?q="+term+"&type=artist");
+
+            request = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .header("Authorization", "Bearer " + token)
+                    .build();
+
+            response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response);
+            System.out.println(response.body());
+
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
